fd63fe79801adc4afc328e99ced90be73f440f88
Eric <eric.mailman@yahoo.com>
2026-04-02 16:39:07 -0400

    Make coin invisible after collision

--- get rich.py
+++ get rich.py
@@ -34,14 +34,14 @@ alien.y = HEIGHT / 2
 
 screen: Screen
 
-# Create a list of coin objects
+# Create a list for the coins
 coins = []
-for i in range(3):
-    coin = Actor("gold_coin")
-    coins.append(coin)
 
 # Create of list of coin booleans to record collisions
 collide_coin = [False, False, False]
+
+# Create a list of booleans to control coin visibility
+visible_coin = [True, True, True]
 
 
 def draw():
@@ -52,8 +52,9 @@ def draw():
 
     alien.draw()
 
-    for _coin in coins:
-        _coin.draw()
+    for i, coin in enumerate(coins):
+        if visible_coin[i]:
+            coin.draw()
 
 
 def on_key_down():
@@ -67,22 +68,33 @@ def on_key_down():
 def update():
     global collide_coin
 
-    for _i, _coin in enumerate(coins):
-        if alien.colliderect(_coin):
-            # Only play the sound once per collision
-            if not collide_coin[_i]:
-                collide_coin[_i] = True
+    for i, coin in enumerate(coins):
+        if alien.colliderect(coin):
+
+            if not collide_coin[i]:
+                # Only play the sound once per collision
+                collide_coin[i] = True
                 sounds.coin.play()
+
+                # Make the coin invisible after collision
+                visible_coin[i] = False
         else:
-            collide_coin[_i] = False
+            collide_coin[i] = False
 
 
 def animate_1():
-    # Animate the first coin in the list
-    _coin = coins[0]
-    _coin.pos = (600, 50)
+    """ Animate the first coin in the list """
+    coin = coins[0]
+
+    # Set the position off the right edge of the screen
+    coin.pos = (600, 50)
+
+    # Make the coin visible
+    visible_coin[0] = True
+
+    # Run the animation
     animate(
-        _coin,
+        coin,
         tween="linear",
         duration=ANIMATE_TIME_1,
         on_finished=animate_1,
@@ -91,11 +103,18 @@ def animate_1():
 
 
 def animate_2():
-    # Animate the second coin in the list
-    _coin = coins[1]
-    _coin.pos = (600, 150)
+    """ Animate the second coin in the list """
+    coin = coins[1]
+
+    # Set the position off the right edge of the screen
+    coin.pos = (600, 150)
+
+    # Make the coin visible
+    visible_coin[1] = True
+
+    # Run the animation
     animate(
-        _coin,
+        coin,
         tween="linear",
         duration=ANIMATE_TIME_2,
         on_finished=animate_2,
@@ -104,11 +123,18 @@ def animate_2():
 
 
 def animate_3():
-    # Animate the third coin in the list
-    _coin = coins[2]
-    _coin.pos = (600, 250)
+    """ Animate the third coin in the list """
+    coin = coins[2]
+
+    # Set the position off the right edge of the screen
+    coin.pos = (600, 250)
+
+    # Make the coin visible
+    visible_coin[2] = True
+
+    # Run the animation
     animate(
-        _coin,
+        coin,
         tween="linear",
         duration=ANIMATE_TIME_3,
         on_finished=animate_3,
@@ -116,8 +142,18 @@ def animate_3():
     )
 
 
-animate_1()
-animate_2()
-animate_3()
+def main():
+    # Store the coin objects in a global list
+    for i in range(3):
+        coin = Actor("gold_coin")
+        coins.append(coin)
 
-pgzrun.go()
+    # Start the animations
+    animate_1()
+    animate_2()
+    animate_3()
+
+    pgzrun.go()
+
+
+main()
